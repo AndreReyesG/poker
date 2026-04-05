6d31e03c3fc12341a19773be19f7f0952f135ec1
AndreReyesG <jandre.reyesg@gmail.com>
2026-04-04 20:41:04 -0600

    refactor: agregar helpers
    Agregar la función newGetScoreRequest y assertResponseBody.

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package poker_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,30 +11,32 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("returns Moka's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Moka", nil)
+		request := newGetScoreRequest("Moka")
 		response := httptest.NewRecorder()
 
 		poker.PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Milky's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Milky", nil)
+		request := newGetScoreRequest("Milky")
 		response := httptest.NewRecorder()
 
 		poker.PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "10")
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
 }
