01c0ca9cb68804275e8ee36bc8d474d767d32435
AndreReyesG <jandre.reyesg@gmail.com>
2026-04-07 18:05:50 -0600

    refactor(webserver): usar r.PathValue

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package poker
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 type PlayerStore interface {
@@ -23,8 +22,9 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 
 	router := http.NewServeMux()
 
-	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
-	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router.Handle("GET /league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("GET /players/{name}", http.HandlerFunc(p.showScoreHandler))
+	router.Handle("POST /players/{name}", http.HandlerFunc(p.processWinHandler))
 
 	p.Handler = router
 
@@ -35,17 +35,8 @@ func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w, player)
-	case http.MethodGet:
-		p.showScore(w, player)
-	}
-}
-
-func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+func (p *PlayerServer) showScoreHandler(w http.ResponseWriter, r *http.Request) {
+	player := r.PathValue("name")
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
@@ -54,7 +45,8 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+func (p *PlayerServer) processWinHandler(w http.ResponseWriter, r *http.Request) {
+	player := r.PathValue("name")
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
