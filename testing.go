62f3366317e25867f0424a6a03de92367c2c46e5
AndreReyesG <jandre.reyesg@gmail.com>
2026-04-15 15:57:15 -0600

    refactor(assertions): mover assertions a un nuevo archivo

--- testing.go
+++ testing.go
@@ -2,9 +2,7 @@ package poker
 
 import (
 	"io"
-	"net/http/httptest"
 	"os"
-	"reflect"
 	"testing"
 )
 
@@ -43,27 +41,6 @@ func CreateTempFile(t testing.TB, initialData string) (*os.File, func()) {
 	return tmpfile, removeFile
 }
 
-func AssertResponseBody(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("response body is wrong, got %q, want %q", got, want)
-	}
-}
-
-func AssertStatus(t testing.TB, got, want int) {
-	t.Helper()
-	if got != want {
-		t.Errorf("did not get correct status, got %d, want %d", got, want)
-	}
-}
-
-func AssertLeague(t testing.TB, got, want []Player) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
 func GetLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
 	league, err := NewLeague(body)
@@ -71,18 +48,4 @@ func GetLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
 	}
 	return
-}
-
-func AssertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
-	t.Helper()
-	if response.Result().Header.Get("content-type") != want {
-		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
-	}
-}
-
-func AssertScoreEquals(t testing.TB, got, want int) {
-	t.Helper()
-	if got != want {
-		t.Errorf("did not get correct score, got %d, want %d", got, want)
-	}
 }
