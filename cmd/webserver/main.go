0df4c3f868068e2ec42f3cb0bb8016152c0970ab
AndreReyesG <jandre.reyesg@gmail.com>
2026-04-20 17:45:57 -0600

    refactor(filesys): encapsulate opening a file

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/AndreReyesG/poker"
 )
@@ -11,18 +10,16 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, closeFile, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 	if err != nil {
-		log.Fatalf("problem opening %s, %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating file system store, %q", err.Error())
-	}
+	defer closeFile()
 
 	server := poker.NewPlayerServer(store)
 
 	log.Print("starting server on :5000")
-	log.Fatal(http.ListenAndServe(":5000", server))
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000, %v", err)
+	}
 }
